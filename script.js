8381d5ee2a1d9db32bd8049d614c6c9af9853806
bachram <jabbarovbahramdev@gmail.com>
2025-02-27 15:21:38 +0400

    loader

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ const quoteText = document.getElementById('quote');
 const quoteAuthor = document.getElementById('author');
 const twitterButton = document.getElementById('twitter');
 const moreWisdomButton = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 let apiQuotes = [];
+
+function onLoading() {
+    quoteContainer.hidden = true;
+    loader.hidden = false;
+}
+
+function completeLoading () {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 
 function newQuote() {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
@@ -14,9 +25,11 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote')
     }
+    completeLoading();
 }
 
 async function getQuotes() {
+    onLoading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
